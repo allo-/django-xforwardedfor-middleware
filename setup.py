f8d6b98e1776dc3d2aa9e99ef91871cd029fc66f
Alexander Schier <allo@laxu.de>
2015-01-19 17:57:38 +0100

    initial commit and version 1.0

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='django-xforwardedfor-middleware',
+      description='Django X-Forwarded-For Middleware',
+      long_description='Use the X-Forwarded-For header to get the real ip of a request',
+      author='Alexander Schier',
+      author_email='allo@laxu.de',
+      url='https://github.com/allo-/django-xforwardedfor-middleware',
+      version='1.0',
+      packages=['x_forwarded_for'],
+      classifiers=[
+          'Framework :: Django',
+          'License :: Public Domain',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python'
+      ]
+      )
+
