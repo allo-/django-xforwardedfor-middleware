d662815b40ed0ae1f37a47bef22cfbab7c61a430
Oliver Marks <oly@digitaloctave.com>
2017-07-23 16:40:56 +0100

    Update middleware.py

--- x_forwarded_for/middleware.py
+++ x_forwarded_for/middleware.py
@@ -1,4 +1,6 @@
-class XForwardedForMiddleware():
+from django.utils.deprecation import MiddlewareMixin
+
+class XForwardedForMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
